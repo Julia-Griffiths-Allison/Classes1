51b51624b0f872568c5fa2150f29f914bc2aa151
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-22 12:22:21 -0400

    first commit, created and initialized a car calss

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+    public class Car
+    {
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        
+    }      
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Classes1;
+
+Car myCar = new Car();
+myCar.Make = "Ford";
+myCar.Model = "Focus";
+myCar.Year = "2017";
+Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} {myCar.Year}.");
